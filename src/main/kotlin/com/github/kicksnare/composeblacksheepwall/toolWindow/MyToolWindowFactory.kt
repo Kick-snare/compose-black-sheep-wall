5bab6e17b8a7d04c09092cd11ccd1a9e3e60f662
GitHub Action <action@github.com>
2024-10-22 13:27:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/kicksnare/composeblacksheepwall/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/kicksnare/composeblacksheepwall/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.kicksnare.composeblacksheepwall.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kicksnare.composeblacksheepwall.MyBundle
+import com.github.kicksnare.composeblacksheepwall.services.MyProjectService
 import javax.swing.JButton
 
 
